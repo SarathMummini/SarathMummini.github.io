6e920cc37cd75f48c81b7096e29e3021cc9c37b1
sarath kumar mummini <sarath.kumar.mummini@sap.com>
2018-04-13 13:35:49 +0530

    concatenating css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ var htmlrender = require('gulp-htmlrender');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var concatCss = require('gulp-concat-css');
+ 
+gulp.task('css', function () {
+  return gulp.src('src/styles/*.css')
+    .pipe(concatCss('styles.css'))
+    .pipe(gulp.dest('dist/styles'));
+});
 
 gulp.task('scripts', function() {
     return gulp.src('src/js/*.js')
